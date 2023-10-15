80e76d2ec8d8f589a0e99cfa9419ace623eeefda
Aldi Triavin <aldi3avin@gmail.com>
2023-10-15 22:03:38 +0700

    add Constants class

---
+++ src/main/java/com/juaracoding/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.juaracoding.utils;
+
+public class Constants {
+    // Global Variable
+    public static final String ANDROID = "android";
+    public static final String DEVICENAME = "CPH1701";
+    public static final String UDID = "87b2c81a";
+    public static final String PLATFORMNAME = "Android";
+    public static final String APPPACKAGE = "com.chad.financialrecord";
+    public static final String APPACTIVITY = "com.rookie.catatankeuangan.feature.splash.SplashActivity";
+    public static final String URL = "http://127.0.0.1:4723/wd/hub";
+    public static final long TIMEOUT = 30;
+}
