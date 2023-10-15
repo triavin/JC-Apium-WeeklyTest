a5c542a03dda57cef300fee256d5fc4b7b40b2be
Aldi Triavin <aldi3avin@gmail.com>
2023-10-16 02:29:45 +0700

    add HomePageElement class

---
+++ src/main/java/com/juaracoding/utils/HomePageElement.java
@@ -0,0 +1,7 @@
+package com.juaracoding.utils;
+
+public class HomePageElement {
+    public static final String BALANCEINFO = "com.chad.financialrecord:id/tvBalance";
+    public static final String EXPENSEINFO = "com.chad.financialrecord:id/tvExpense";
+    public static final String INCOMEINFO = "com.chad.financialrecord:id/tvIncome";
+}
