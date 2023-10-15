8cf8271577bd96efbb1d2aa9634a4b3ddc18dd4e
Aldi Triavin <aldi3avin@gmail.com>
2023-10-16 00:45:52 +0700

    update Main class

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -1,6 +1,7 @@
 package com.juaracoding;
 
 import com.juaracoding.driver.DriverSingleton;
+import com.juaracoding.driver.pages.AddTransaksi;
 import com.juaracoding.utils.Constants;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
@@ -14,7 +15,7 @@ import java.net.URL;
 public class Main {
     public static AndroidDriver driver;
     public static void main(String[] args) throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        //DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
         // URL
         //URL url = new URL("http://127.0.0.1:4723/wd/hub");
@@ -22,11 +23,22 @@ public class Main {
         // instance AndroidDriver
        // AndroidDriver driver = new AndroidDriver<>(url,desiredCapabilities);
         DriverSingleton.getInstance(Constants.ANDROID);
-        driver = DriverSingleton.getDriver();
-        // Mobile Element
-        MobileElement add = (MobileElement) driver.findElement(By.id("com.chad.financialrecord:id/fabMenu"));
 
-        add.click();
+        //driver = DriverSingleton.getDriver();
+        // Mobile Element
+        //MobileElement add = (MobileElement) driver.findElement(By.id("com.chad.financialrecord:id/fabMenu"));
+
+        //add.click();
+
+        AddTransaksi addTransaksi = new AddTransaksi();
+        DriverSingleton.delay(5);
+        System.out.println("print after delay");
+        addTransaksi.addData();
+        DriverSingleton.delay(1);
+        addTransaksi.getElement();
+        System.out.println("get element");
+        DriverSingleton.delay(1);
+        addTransaksi.AddIncome("1000","Test");
 
         //
         // driver.quit();
